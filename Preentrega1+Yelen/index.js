b5feacfec3baacecffd0c2a5a4965435954a012c
Martin Yelen <yomaryelen_14@hotmail.com>
2022-12-12 22:28:06 -0300

    actualizacion

--- Preentrega1+Yelen/index.js
+++ Preentrega1+Yelen/index.js
@@ -1,11 +1,21 @@
 const vinos = [
-    {nombre: 'Trumpeter', cepa: 'malbec', precio: 1000},
-    {nombre: 'Alma mora', cepa: 'syrah', precio: 800},
-    {nombre: 'Luigi bosca', cepa: 'cabernet', precio: 1500},
-    {nombre: 'Enemgio', cepa: 'malbec', precio: 1200},
-    {nombre: 'Ruttini', cepa: 'cabernet', precio: 2000},
-    {nombre: 'Elementos', cepa: 'syrah', precio: 750},
+    {id: 1, nombre: 'Trumpeter', cepa: 'malbec', precio: 1000},
+    {id: 2, nombre: 'Alma mora', cepa: 'syrah', precio: 800},
+    {id: 3, nombre: 'Luigi bosca', cepa: 'cabernet', precio: 1500},
+    {id: 4, nombre: 'Enemgio', cepa: 'malbec', precio: 1200},
+    {id: 5, nombre: 'Ruttini', cepa: 'cabernet', precio: 2000},
+    {id: 6, nombre: 'Catena Zapata', cepa: 'malbec', precio: 1750},
+    {id: 7, nombre: 'Zucardi piedra', cepa: 'malbec', precio: 2000},
+    {id: 8, nombre: 'Estiba', cepa: 'malbec', precio: 550},
+    {id: 9, nombre: 'Trapiche iscay', cepa: 'syrah', precio: 1550},
+    {id: 10, nombre: 'Finca las moras', cepa: 'syrah', precio: 1850},
+    {id: 11, nombre: 'Sophenia', cepa: 'syrah', precio: 450},
+    {id: 12, nombre: 'Bianchi', cepa: 'cabernet', precio: 1500},
+    {id: 13, nombre: 'Primus', cepa: 'cabernet', precio: 400},
 ];
+
+
+
 let mensaje = "Estos son los vinos disponibles para su seleccion:\n";
 let resultado = [];
 let limite_precio;
@@ -55,3 +65,35 @@ function evaluar_precio(precio){
     }
    
 }
+
+
+
+
+
+
+
+/*let encontrado = vinos.find(item => vinos.nombre === "trumpeter");
+let mensaje =
+id: ${encontrado.id}
+nombre: ${encontrado.nombre} //xq esta en gris
+Precio: $${encontrado.precio}
+;
+console.log(encontrado); //puede ser con un alert tmb?
+
+/////////////////////////////////////////////////7
+
+let filtrados = vinos.filter ((item) => vinos.precio > precio );
+filtrados.forEach((item) => {
+    let mensaje = `
+    id: ${item.id}
+    nombre: ${item.nombre}
+    precio: $${item.precio}
+    `;
+
+    alert(mensaje)
+    console.log(filtrados)
+
+});
+
+
+
